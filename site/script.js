58eb57071fc0293daabc80c074531a57e4916336
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-02-20 02:35:54 -0300

    terminei o basico do presente da bia

--- site/script.js
+++ site/script.js
@@ -5,13 +5,21 @@ const degrade = document.querySelector('.rectangle-11')
 const foto = document.querySelector('.foto')
 const section1 = document.querySelector('.section1')
 const section2 = document.querySelector('.section2')
+const modal = document.querySelector('#audios')
+const closeBtn = document.querySelector('#close img');
+const closeBtn2 = document.querySelector('#close2 img');
+const modal2 = document.querySelector('#escrita')
+const senha = document.querySelector('.senha')
+const codigo = document.querySelector('#senha')
 
+
+senha.classList.add('hidden')
 section1.classList.add('hidden')
 section2.classList.add('hidden')
 degrade.classList.add('hidden')
 foto.classList.add('hidden')
 
-mudarPg.addEventListener('click', function() {
+mudarPg.addEventListener('click', function () {
     ola.classList.add('hidden')
     clique.classList.add('hidden')
     degrade.classList.remove('hidden')
@@ -26,3 +34,21 @@ mudarPg.addEventListener('click', function() {
         section2.classList.add('show');
     }, 10);
 })
+
+document.querySelector('#homenagens').addEventListener('click', () => {
+    modal.showModal()
+    document.body.classList.add('dialog-open');
+})
+closeBtn.addEventListener('click', () => {
+    modal.close();
+    document.body.classList.remove('dialog-open');
+});
+
+document.querySelector('#poesia').addEventListener('click', () => {
+    modal2.showModal()
+    document.body.classList.add('dialog-open');
+})
+closeBtn2.addEventListener('click', () => {
+    modal2.close();
+    document.body.classList.remove('dialog-open');
+});
