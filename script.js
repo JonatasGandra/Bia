16dd5eca7828e4de14232bb845fcccdcac4837e8
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-02-22 02:44:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const mudarPg = document.getElementsByTagName('body')[0]
 const ola = document.querySelector('.ola')
 const clique = document.querySelector('h2')
-const degrade = document.querySelector('.rectangle-11')
 const foto = document.querySelector('.foto')
 const section1 = document.querySelector('.section1')
 const section2 = document.querySelector('.section2')
@@ -17,7 +16,6 @@ const digite = document.querySelector('.senha h1')
 senha.classList.add('hidden')
 section1.classList.add('hidden')
 section2.classList.add('hidden')
-degrade.classList.add('hidden')
 foto.classList.add('hidden')
 
 mudarPg.addEventListener('click', function () {
@@ -38,16 +36,13 @@ entrar.addEventListener('click', () => {
         codigo.classList.add('hidden')
         entrar.classList.add('hidden')
         digite.classList.add('hidden')
-        degrade.classList.remove('hidden')
         ola.classList.add('hidden')
         clique.classList.add('hidden')
-        degrade.classList.remove('hidden')
         foto.classList.remove('hidden')
         section1.classList.remove('hidden')
         section2.classList.remove('hidden')
 
         setTimeout(() => {
-            degrade.classList.add('show');
             foto.classList.add('show');
             section1.classList.add('show');
             section2.classList.add('show');
