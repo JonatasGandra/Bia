b1fe6bdde85ae1c9cfff739e43f068556d5d5162
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-02-22 13:29:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const entrar = document.querySelector('#entrar')
 
 entrar.addEventListener('click', () => {
 
-    if (codigo.value === 'bialinda') {
+    if (codigo.value === 'biamaravilhosa') {
 
         codigo.classList.add('hidden')
         entrar.classList.add('hidden')
